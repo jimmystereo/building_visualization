9bb987cf0afe34fa83979d6038c119a658030fc6
jimmystereo <61770217+jimmystereo@users.noreply.github.com>
2023-10-14 22:13:30 -0500

    update

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,7 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 df = pd.read_csv('building_inventory.csv', na_values = {'Year Acquired':0,'Year Constructed':0})
-
+df['Year Acquired'] = pd.to_datetime(df['Year Acquired'] )
+df['Year Constructed']= pd.to_datetime(df['Year Constructed'])
 df.columns
 df = df.dropna(subset=['Year Acquired', 'Year Constructed'])
 df.to_csv('bd_data.csv',encoding='utf-8-sig', index=False)
 df
+
+plt.scatter(df['Year Acquired'], df['Year Constructed'])
+
+df['Year Acquired']
+
+
