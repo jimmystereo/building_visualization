63d7c1139fb579fd7224bea6f466f3d0b7a4e27d
jimmystereo <61770217+jimmystereo@users.noreply.github.com>
2023-10-14 21:54:05 -0500

    update

---
+++ data_preprocess.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df = pd.read_csv('building_inventory.csv', na_values = {'Year Acquired':0,'Year Constructed':0})
+
+df.columns
+df = df.dropna(subset=['Year Acquired', 'Year Constructed'])
+df.to_csv('bd_data.csv',encoding='utf-8-sig', index=False)
